c3b2c37c7ae0799a452046436ff230fb5aa40f1c
Evgeniy <153195535+cyberrunner0@users.noreply.github.com>
2024-07-24 19:28:50 +0300

    Upload script. Version 1
    You must enter the name of the user whose groups you want to copy and the user to whom you want to copy.

---
+++ TransferOfGroups.ps1
@@ -0,0 +1,53 @@
+﻿# Import the Active Directory module
+Import-Module ActiveDirectory
+
+# Function to get user groups
+function Get-UserGroups {
+    param (
+        [string]$username
+    )
+
+    $user = Get-ADUser -Identity $username -Properties MemberOf
+    $groups = @()
+
+    if ($user -ne $null) {
+        $user.MemberOf | ForEach-Object {
+            $group = Get-ADGroup -Identity $_
+            $groups += $group.Name
+        }
+    }
+
+    return $groups
+}
+
+# Function to set user groups
+function Set-UserGroups {
+    param (
+        [string]$username,
+        [array]$groups
+    )
+
+    $currentGroups = Get-UserGroups -username $username
+    $groupsToAdd = $groups | Where-Object { $currentGroups -notcontains $_ }
+    $groupsToRemove = $currentGroups | Where-Object { $groups -notcontains $_ }
+
+    foreach ($group in $groupsToAdd) {
+        Add-ADGroupMember -Identity $group -Members $username
+    }
+
+    foreach ($group in $groupsToRemove) {
+        Remove-ADGroupMember -Identity $group -Members $username
+    }
+}
+
+# Get the reference (source) and target users
+$referenceUser = Read-Host "Enter the reference (source) username"
+$targetUser = Read-Host "Enter the target username"
+
+# Get groups of the reference user
+$referenceUserGroups = Get-UserGroups -username $referenceUser
+
+# Set groups to the target user
+Set-UserGroups -username $targetUser -groups $referenceUserGroups
+
+Write-Host "Groups from $referenceUser have been copied to $targetUser."
